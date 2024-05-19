8c1ccbae1091932fa4983db509e238a120a93fcf
Vansh810 <vansh.agarwal@gmail.com>
2024-05-19 14:21:17 +0530

    Added Model Training and Evaluation

--- AgeDetection.py
+++ AgeDetection.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import cv2
 import os
+from matplotlib import pyplot as plt
 from glob import glob
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from keras.models import  Sequential
@@ -23,8 +24,7 @@ ages = []
 
 for path in all_images:
     img = cv2.imread(path)
-    if img.size != (200, 200):
-        img = cv2.resize(img, (200, 200))
+    img = cv2.resize(img, (128, 128))
     images.append(img)
     ages.append(get_age(path))
 
@@ -36,3 +36,39 @@ images = images / 255
 
 X_train, X_test, y_train, y_test = train_test_split(images, ages, test_size=0.2)
 
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(1)  # Single output for age regression
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True
+)
+datagen.fit(X_train)
+
+history = model.fit(datagen.flow(X_train, y_train, batch_size=32), epochs=1, validation_data=(X_test, y_test))
+
+loss, mae = model.evaluate(X_test, y_test)
+print(f'Mean Absolute Error: {mae}')
+
+plt.plot(history.history['mae'], label='MAE (training)')
+plt.plot(history.history['val_mae'], label='MAE (validation)')
+plt.xlabel('Epoch')
+plt.ylabel('Mean Absolute Error')
+plt.legend()
+plt.show()
+
+model.save('age_detection_model.h5')
